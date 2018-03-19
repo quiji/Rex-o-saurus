33f502a022ab4c389a4df51f312c4503861082d2
quiji <erick.quijivix@gmail.com>
2018-03-19 13:04:26 -0300

    Implemented Idle and Run

--- rex/gen_rex_sprite.gd
+++ rex/gen_rex_sprite.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+enum ReactTypes {NO_REACTION, STEP_REACTION}
+
 var current_anim = ''
 
 func _ready():
@@ -9,10 +11,19 @@ func _ready():
 
 func idle():
 	play("Idle")
-	
+
+func run():
+	play("Run")
+
+func react(action):
+	match action:
+		STEP_REACTION:
+			get_parent().add_step_impulse()
 	
 func play(anim):
 	if current_anim != anim:
 		current_anim = anim
 		$anim_player.play(anim)
 
+func flip(left=true):
+	$sprite.flip_h = left
